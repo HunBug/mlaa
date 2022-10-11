d5a36d695d6f6eda9ae7368bdbc453f511b75cb5
astrackomni <69056244+astrackomni@users.noreply.github.com>
2022-10-11 22:08:49 +0200

    Working python trainer demo

---
+++ src/MLAA/NeuralNetwork/PythonRunner.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    internal class PythonRunner
+    {
+        private readonly ILogger<PythonRunner> logger;
+        public string PythonExecutablePath { get; private set; } = "python";
+        public string PythonScriptPath { get; private set; } = "";
+        public PythonRunner(string pythonExecutablePath, string pythonScriptPath, ILogger<PythonRunner>? logger = null)
+        {
+            PythonExecutablePath = pythonExecutablePath;
+            PythonScriptPath = pythonScriptPath;
+            if (logger != null)
+            {
+                this.logger = logger;
+            }
+            else
+            {
+                this.logger = new LoggerFactory().CreateLogger<PythonRunner>();
+            }
+            
+        }
+
+        public void Run(IDictionary<string, string> args)
+        {
+            var arguments = new List<string>();
+            foreach (var arg in args)
+            {
+                arguments.Add($"--{arg.Key}");
+                arguments.Add($"{arg.Value}");
+            }
+            Run(arguments.ToArray());
+        }
+
+        public void Run(object args)
+        {
+            var arguments = new Dictionary<string, string>();
+            foreach (var arg in args.GetType().GetProperties())
+            {
+                var argValue = arg.GetValue(args)?.ToString();
+                if (argValue != null)
+                {
+                    arguments.Add(arg.Name, argValue);
+                }
+                else
+                {
+                    logger.LogWarning("Argument {ArgName} is null", arg.Name);
+                }
+            }
+            Run(arguments);
+        }
+
+        public void Run(string[] args)
+        {
+            var arguments = new StringBuilder();
+            foreach (var arg in args)
+            {
+                arguments.Append($"\"{arg}\" ");
+            }
+            var process = new Process();
+            process.StartInfo.FileName = PythonExecutablePath;
+            process.StartInfo.Arguments = $"\"{PythonScriptPath}\" {arguments}";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+
+            //create event handler
+            // Reading synchronously from the output,error stream of the process can cause the process to hang.
+            process.OutputDataReceived += new DataReceivedEventHandler(
+                (s, e) =>
+                {
+                    //do something with the output data 'e.Data'
+                    logger.LogInformation("Python output: {PythonOutput}", e.Data);
+                }
+            );
+            process.ErrorDataReceived += new DataReceivedEventHandler(
+                (s, e) =>
+                {
+                    logger.LogError("Python error: {ErrorData}", e.Data);
+                }
+            );
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+            if (process.ExitCode != 0)
+            {
+                logger.LogError("Python process exited with code {ExitCode}", process.ExitCode);
+                throw new ApplicationException($"Python script exited with error code {process.ExitCode}. See output for more information.");
+            }
+        }
+    }
+
+}
---
+++ src/MLAA/Python/ObjectDetection/ObjectDetection.py
@@ -0,0 +1,124 @@
+from typing import Any
+import numpy as np
+import tensorflow as tf
+
+DEBUG = True
+if (DEBUG and __name__ == "__main__"):
+    # dont forget to use @tf.function decorator!
+    tf.config.run_functions_eagerly(True)
+    tf.config.experimental_run_functions_eagerly(True)
+    tf.data.experimental.enable_debug_mode()
+
+csv_path = "./killfeed_data.csv"
+
+
+@tf.function
+def load_image(features):
+    new_feats = {}
+    for name, value in features.items():
+        if name.endswith("path"):
+            filecontent = tf.io.read_file(tf.reshape(value, []))
+            new_feats[name] = tf.image.decode_jpeg(filecontent, channels=3)
+        else:
+            new_feats[name] = value
+    return new_feats
+
+
+@tf.function
+def supervised_labels_mapper(example: dict[str, Any]) -> tuple[dict[str, Any], dict[str, Any]]:
+    example_inputs: dict[str, Any] = dict()
+    input_layer_name = "sample_path"
+    example_inputs[input_layer_name] = example[input_layer_name]
+    example_outputs: dict[str, Any] = dict()
+    output_layer_names = ["weapon"]
+    for layer_name in output_layer_names:
+        example_outputs[layer_name] = example[layer_name]
+    return example_inputs, example_outputs
+
+
+raw_dataset = tf.data.experimental.make_csv_dataset(
+    csv_path,
+    batch_size=1,  # fir simple mapping (string handling).
+    num_epochs=1,
+    ignore_errors=False,)
+
+for features in raw_dataset.take(1):
+    for i, (name, value) in enumerate(features.items()):
+        if i > 15:
+            break
+        print(f"{name:20s}: {value}")
+print('...')
+print(f"[total: {len(features)} features]")
+
+images_dataset = raw_dataset.map(load_image)
+
+for features in images_dataset.take(1):
+    for i, (name, value) in enumerate(features.items()):
+        if i > 15:
+            break
+        print(f"{name:20s}: {value}")
+print('...')
+print(f"[total: {len(features)} features]")
+
+
+images_dataset = images_dataset.map(supervised_labels_mapper)
+labels = np.concatenate([x["weapon"] for x in raw_dataset], axis=0)
+lookup = tf.keras.layers.StringLookup(vocabulary=np.unique(labels))
+one_hot = tf.keras.layers.CategoryEncoding(num_tokens=lookup.vocabulary_size())
+images_dataset = images_dataset.map(
+    lambda x, y: (x, {"weapon": one_hot(lookup(y["weapon"]))}))
+for input, output in images_dataset.batch(6).take(1):
+    for i, (name, value) in enumerate(input.items()):
+        if i > 15:
+            break
+        print(f"input-{name:20s}: {value}")
+    print(output)
+    for i, (name, value) in enumerate(output.items()):
+        if i > 15:
+            break
+        print(f"output-{name:20s}: {value}")
+print('...')
+print(f"[total: {len(features)} features]")
+
+input = tf.keras.layers.Input((46, 600, 3), name="sample_path")
+encoder = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(filters=8, kernel_size=(3, 3), strides=(1, 1), dilation_rate=(
+        1, 1), padding="same", use_bias=True, kernel_initializer=tf.keras.initializers.GlorotUniform(),
+        activation="relu"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(pool_size=(
+        2, 2), padding="same", strides=(2, 2)),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), strides=(1, 1), dilation_rate=(
+        1, 1), padding="same", use_bias=True, kernel_initializer=tf.keras.initializers.GlorotUniform(),
+        activation="relu"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(pool_size=(
+        2, 2), padding="same", strides=(2, 2)),
+    tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), dilation_rate=(
+        1, 1), padding="same",  use_bias=True, kernel_initializer=tf.keras.initializers.GlorotUniform(),
+        activation="relu"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(pool_size=(
+        2, 2), padding="same", strides=(2, 2)),
+    tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), dilation_rate=(
+        1, 1), padding="same",  use_bias=True, kernel_initializer=tf.keras.initializers.GlorotUniform(),
+        activation="relu"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(pool_size=(
+        2, 2), padding="same", strides=(2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, kernel_initializer=tf.keras.initializers.GlorotUniform(),
+                          activation="sigmoid"),
+])
+encoded_features = encoder(input)
+output = tf.keras.layers.Dense(units=22, kernel_initializer=tf.keras.initializers.GlorotUniform(
+), activation="softmax", name="weapon")(encoded_features)
+model = tf.keras.Model(inputs=input, outputs=output)
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+    metrics=["accuracy"],
+)
+model.summary()
+
+model.fit(images_dataset.batch(32), epochs=5)
