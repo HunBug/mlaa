928167af36ef58704768961f40a8ce0551417e88
astrackomni <69056244+astrackomni@users.noreply.github.com>
2022-10-11 11:49:10 +0200

    Latest state of (abadnoned) 3rd party tf integration

---
+++ src/MLAA/NeuralNetwork/Model/Encoder/Cnn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Tensorflow.KerasApi;
+using Tensorflow;
+using Tensorflow.Keras.Engine;
+using Tensorflow.Keras.Layers;
+using Tensorflow.Keras.Utils;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NeuralNetwork.Model.Encoder
+{
+    internal static class Cnn
+    {
+        public static Functional GetModel(Shape inputShape, int numberOfLayers)
+        {
+            var layers = new LayersApi();
+            // input layer
+            var inputs = keras.Input(shape: inputShape, name: "ff");
+            var x = inputs;
+            // convolutional layers
+            int currentFilterSize = 16;
+            for (int i = 0; i < numberOfLayers - 1; i++)
+            {
+                x = layers.Conv2D(currentFilterSize, 3, activation: "relu").Apply(x);
+                x = layers.MaxPooling2D(3).Apply(x);
+                currentFilterSize *= 2;
+            }
+            var outputs = layers.Conv2D(currentFilterSize, 3, activation: "relu").Apply(x);
+            
+            // build keras model
+            return keras.Model(inputs, outputs, name: "gg");
+        }
+    }
+}
